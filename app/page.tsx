672e9a040c5748a4cf9c235cea92bdaa8adaadfa
89059043673d-ctrl <89059043673d@gmail.com>
2025-08-19 21:13:47 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
-import Chat from '../components/Chat';export default function Page(){return <Chat/>}
+// app/page.tsx
+"use client";
+import Chat from "@/components/Chat";
+import Sidebar from "@/components/Sidebar";
+import { ChatProvider } from "@/components/ChatContext";
+
+export default function Page() {
+  return (
+    <ChatProvider>
+      <div className="shell">
+        <Sidebar />
+        <Chat />
+      </div>
+    </ChatProvider>
+  );
+}
