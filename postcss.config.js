2aa8b44e3505c588b4eecfdcb784c3ef4298da4f
89059043673d-ctrl <89059043673d@gmail.com>
2025-08-20 06:33:17 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ['class'],
-  content: [
-    './app/**/*.{ts,tsx,js,jsx,mdx}',
-    './components/**/*.{ts,tsx,js,jsx,mdx}',
-  ],
-  theme: {
-    extend: {},
+// postcss.config.js (ESM-версия)
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
