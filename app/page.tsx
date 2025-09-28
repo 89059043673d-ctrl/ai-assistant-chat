c05f9621b80174b53c55d522f3e89fd4c4c3e86e
89059043673d-ctrl <89059043673d@gmail.com>
2025-09-28 18:45:55 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Chat from '@/components/Chat';
 
 export default function Page() {
-  return (
-    <div className="min-h-[100dvh] bg-zinc-950 text-zinc-100">
-      <Chat />
-    </div>
-  );
+  return <Chat />;
 }
