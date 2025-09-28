878ec05e07bfa5169d9f70bc24beed19c3c0849b
89059043673d-ctrl <89059043673d@gmail.com>
2025-09-28 19:19:53 +0300

    Update Markdown.tsx

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,11 +1,16 @@
 'use client';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default function Markdown({ children }: { children: string }) {
   return (
     <ReactMarkdown
+      remarkPlugins={[remarkMath]}
+      rehypePlugins={[rehypeKatex]}
       components={{
+        /* жирный/курсив/заголовки остаются по умолчанию */
         code: ({ inline, children }) =>
           inline ? (
             <code className="px-1 py-0.5 rounded bg-zinc-200 text-zinc-900">{children}</code>
@@ -19,6 +24,9 @@ export default function Markdown({ children }: { children: string }) {
             {children}
           </a>
         ),
+        /* списки чуть компактнее */
+        ul: ({ children }) => <ul className="list-disc pl-5 space-y-1">{children}</ul>,
+        ol: ({ children }) => <ol className="list-decimal pl-5 space-y-1">{children}</ol>,
       }}
     >
       {children}
