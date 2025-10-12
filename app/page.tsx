89c5f6244c1afa415e3e256bd0a8bffec21d9db5
89059043673d-ctrl <89059043673d@gmail.com>
2025-10-12 21:33:06 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Chat from "@/components/Chat";
+import Chat from '@/components/Chat';
 
 export default function Page() {
-  return <Chat messages={[]} showWelcome={true} />;
+  return <Chat />;
 }
